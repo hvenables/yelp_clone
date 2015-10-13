23e352cda689ad0a1771dc265d5097e63c5c9013
MoeSadoon <msadoon590@gmail.com>
2015-10-13 15:29:48 +0100

    added validations

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,3 +3,14 @@
 # RSpec.describe Review, type: :model do
 #   pending "add some examples to (or delete) #{__FILE__}"
 # end
+
+
+require 'rails_helper'
+
+describe Review, :type => :model do
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
+
+end
