d5e338dd6a77f939fd030e50a2e5e6c4213ffb9c
Dom Batten <dominic.batten@googlemail.com>
2015-10-14 13:36:31 +0100

    Added env variable for facebook app id and secret

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
-  # get 'restaurants' => 'restaurants#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } 
+   # get 'restaurants' => 'restaurants#index'
   root to: "restaurants#index"
   resources :restaurants do
     resources :reviews
