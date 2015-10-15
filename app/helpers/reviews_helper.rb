8d9c4208d69cc4b31f683b827199860ddfdb8109
Emily Worrall <worrall.emily@gmail.com>
2015-10-15 16:37:17 +0100

    Average rating displayed, with stars

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,5 +1,7 @@
 module ReviewsHelper
   def star_rating(rating)
-    rating
+    return rating unless rating.respond_to?(:round)
+    remainder = (5 - rating)
+    '★' * rating.round + "☆" * remainder
   end
 end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -7,8 +7,16 @@ describe ReviewsHelper, :type => :helper do
       expect(helper.star_rating('N/A')).to eq 'N/A'
     end
 
-    it 'returns five black stars for five' do 
+    it 'returns five black stars for five' do
       expect(helper.star_rating(5)).to eq '★★★★★'
+    end
+
+    it 'returns three black stars and two white stars for three' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
+    it 'returns four black stars and one white star for 3.5' do
+      expect(helper.star_rating(3.5)).to eq '★★★★☆'
     end
   end
 end
