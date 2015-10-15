2dde681de7ebd560ca7080ff8ddd6b50f7a3a22b
Emily Worrall <worrall.emily@gmail.com>
2015-10-15 15:40:40 +0100

    Average rating tests in progress:

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,9 @@ class Restaurant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
 
   belongs_to :user
+
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end
 end
