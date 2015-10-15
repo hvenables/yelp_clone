b5c03e2d9fdd711b4f2e430a22c9eeefb2ff25fc
Dom Batten <dominic.batten@googlemail.com>
2015-10-15 15:57:20 +0100

    added average rating method

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,6 @@ class Review < ActiveRecord::Base
 
   validates :rating, inclusion: (1..5)
   # validates :user, uniqueness: { scope: :restaurant, message: "has reviewed this restaurant already" }
-  validates :user_id, :uniqueness => { :scope => :restaurant }
+  validates :user_id, :uniqueness => { :scope => :restaurant_id}
 
 end
