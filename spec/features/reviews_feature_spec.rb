7a33677ce04a4592591bb0a7c60a94c51add04d3
Dom Batten <dominic.batten@googlemail.com>
2015-10-15 15:27:30 +0100

    refactored reviews feature spec

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -2,64 +2,59 @@ require 'rails_helper'
 
 feature 'reviewing' do
 
-  before do
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review The Ox'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
+  def sign_up_with(email)
     visit('/')
     click_link('Sign up')
-    fill_in('Email', with: 'test@example.com')
+    fill_in('Email', with: email)
     fill_in('Password', with: 'testttest')
     fill_in('Password confirmation', with: 'testttest')
     click_button('Sign up')
   end
 
+
+  before do
+    sign_up_with('test@example.com')
+  end
+
   before {Restaurant.create name: 'The Ox'}
 
   scenario 'allows user to leave a review using a form' do
-    visit '/restaurants'
-    click_link 'Review The Ox'
-    fill_in 'Thoughts', with: 'so so'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
-
+    leave_review("so so", 3)
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content 'so so'
   end
 
   scenario 'user can only leave one review per restaurant' do
-    visit '/restaurants'
-    click_link 'Review The Ox'
-    fill_in 'Thoughts', with: 'so so'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+    leave_review("so so", 3)
     click_link 'Review The Ox'
     click_button 'Leave Review'
     expect(page).to have_content('You have already reviewed this restaurant')
   end
 
   scenario 'user can delete a review' do
-    visit '/restaurants'
-    click_link 'Review The Ox'
-    fill_in 'Thoughts', with: 'so so'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+    leave_review("so so", 3)
     click_link 'Delete Review for The Ox'
     expect(page).not_to have_content('so so')
     expect(page).to have_content('Review deleted successfully')
   end
 
   scenario 'user can delete only their own reviews' do
-    visit '/restaurants'
-    click_link 'Review The Ox'
-    fill_in 'Thoughts', with: 'so so'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+    leave_review("so so", 3)
     click_link 'Sign out'
-    click_link 'Sign up'
-    fill_in('Email', with: 'emily@example.com')
-    fill_in('Password', with: 'testttest')
-    fill_in('Password confirmation', with: 'testttest')
-    click_button 'Sign up'
+    sign_up_with('emily@example.com')
     expect(page).not_to have_content('Delete Review for The Ox')
   end
+
+
+  # scenario ''
 
 
 end
