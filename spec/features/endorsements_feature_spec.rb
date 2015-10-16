725754f394bb1f76c0ac2f224af2d9a7448d2f31
Harry Venables <venablesh@yahoo.co.uk>
2015-10-16 15:52:09 +0100

    Got endorsements to work via ajax request

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-feature 'endorsing reviews' do 
+feature 'endorsing reviews' do
 
-  before do 
+  before do
     kfc = Restaurant.create(name: 'KFC')
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
-  scenario 'a user can endorse a review, which update the review endorsements count', js: true do 
+  scenario 'a user can endorse a review, which update the review endorsements count', js: true do
     visit '/restaurants'
     click_link 'Endorse'
     expect(page).to have_content('1 endorsement')
